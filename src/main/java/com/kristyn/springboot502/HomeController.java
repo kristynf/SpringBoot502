b053053e8b67592aa959d2999152697f981566a7
kristynf <kristynf@bu.edu>
2020-03-02 12:06:37 -0500

    initial commit

---
+++ src/main/java/com/kristyn/springboot502/HomeController.java
@@ -0,0 +1,12 @@
+package com.kristyn.springboot502;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/test/java/com/kristyn/springboot502/Springboot502ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristyn.springboot502;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot502ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
